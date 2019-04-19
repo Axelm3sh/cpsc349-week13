43b7b93ab3cd258534100fdc7f53e539afd71405
Axelm3sh <axel.m3sh@gmail.com>
2019-04-18 18:46:12 -0700

    Added code, need to fix element overriding info

--- comments.js
+++ comments.js
@@ -1,3 +1,60 @@
+var $docBody = $(document.body);
+
+//*******AJAX stuff*******
+function getPosts(callback) {
+    console.log("Requesting Posts");
+
+    var params = {
+        url: "https://jsonplaceholder.typicode.com/posts",
+        method: "GET",
+        //        data: params,
+        dataType: "json"
+    };
+
+    $.ajax(params).done(function (results) {
+        if (callback) {
+            //console.log(results);
+            callback.call(this, results);
+        }
+    });
+
+}
+
+function getComments(postId, callback) {
+    console.log("Requesting Comments");
+
+    var params = {
+        url: "https://jsonplaceholder.typicode.com/comments?postId=" + postId,
+        method: "GET",
+        //        data: params,
+        dataType: "json"
+    };
+
+    $.ajax(params).done(function (results) {
+        if (callback) {
+            //console.log(results);
+            callback.call(this, results);
+        }
+    });
+
+}
+
+//*******On Ready********
+$docBody.ready(function (e) {
+
+    getPosts(function (results) {
+
+        results.forEach(function (item) {
+            console.log(item);
+            CreateNewPost(item.title, item.body, item.id);
+
+        });
+
+    });
+
+});
+
+
 (function (window) {
     'use strict';
 
@@ -26,15 +83,77 @@
 })(window);
 
 
-var $docObj = $(document.body);
+//id is the unique postId
+function CreateNewPost(title, body, id) {
+    var template = $("#templatePost").text();
+    console.log(template);
 
-$docObj.ready(function (e) {   
-    
-    
-    
-    $.ajax({
-        url: "https://jsonplaceholder.typicode.com/posts",
-        method: "GET",
-        dataType: "json"
-    });
-});
+    //    return template;
+    $("body").append(template);
+
+    var $article = $("article:last-of-type");
+
+    $article.find("h2").html(title);
+
+    body = body.replace("\n", "<br>");
+
+    $article.find("p").html(body);
+
+    $article.find("button").attr("value", id);
+    $article.find("button").attr("onClick", "ToggleComments(this)");
+
+    //We place our comments in this
+    $article.find("section").attr("id", "comments-" + id);
+
+}
+
+//comments belong to a post, postId identifies it.
+function CreateNewComment(body, name, email, postId) {
+    var template = $("#templateComment").text();
+
+    var $section = $("#comments-" + postId);
+    $section.append(template);
+
+    body = body.replace("\n", "<br>");
+
+    //TODO LAST OF TYPE
+    $section.find("p").html(body);
+    $section.find("a").attr("href", email);
+    $section.find("a").html(name);
+
+}
+
+
+function ToggleComments(elem) {
+
+    //    console.log($(elem).val());
+    var postId = $(elem).val();
+    var commentSect = $("#comments-" + postId);
+
+    //Comments title only? First time check
+    if (commentSect.children().length <= 1) {
+        //Load comments
+        getComments(postId, function (results) {
+            //after results come back, do stuff
+            results.forEach(function (item) {
+                CreateNewComment(item.body, item.name, item.email, postId);
+            });
+
+            //Should only run first time comment load
+            commentSect.slideDown("fast");
+        });
+    } else {
+        
+        if (commentSect.is(":hidden")) 
+        {
+            commentSect.slideDown("fast");
+        }
+        else
+        {
+            commentSect.slideUp("fast");
+        }
+    }
+
+
+
+}
